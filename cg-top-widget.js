f4593189d3cea67909568f2bce1a600968d1f549
TXG Ryan <69308832+TXG-Ryan@users.noreply.github.com>
2023-03-02 13:10:27 +0000

    Update cg-top-widget.js

--- cg-top-widget.js
+++ cg-top-widget.js
@@ -1,26 +1,20 @@
-// define IDs for each element where we'll render the fetched data
-const marketCapId = "market-cap";
-const btcDominanceId = "btc-dominance";
-const ethDominanceId = "eth-dominance";
-const fearGreedIndexId = "fear-greed-index";
-
-// define the API endpoint for fetching data
-const apiEndpoint = "https://api.coingecko.com/api/v3/global";
-
-// use Fetch API to get data from the endpoint
-fetch(apiEndpoint)
+fetch('https://api.coingecko.com/api/v3/global')
   .then(response => response.json())
   .then(data => {
-    // extract the desired data from the API response
     const marketCap = data.data.total_market_cap.usd;
     const btcDominance = data.data.market_cap_percentage.btc;
     const ethDominance = data.data.market_cap_percentage.eth;
-    const fearGreedIndex = data.data.fear_and_greed_index.value;
+    const fearGreedIndex = data.data.fear_and_greed_index;
 
-    // render the data to the DOM via their respective IDs
-    document.getElementById(marketCapId).innerHTML = `$${marketCap.toLocaleString()}`;
-    document.getElementById(btcDominanceId).innerHTML = `${btcDominance.toFixed(2)}%`;
-    document.getElementById(ethDominanceId).innerHTML = `${ethDominance.toFixed(2)}%`;
-    document.getElementById(fearGreedIndexId).innerHTML = fearGreedIndex;
+    document.getElementById('market-cap').innerHTML = `$${marketCap.toLocaleString()}`;
+    document.getElementById('btc-dominance').innerHTML = `${btcDominance.toFixed(2)}%`;
+    document.getElementById('eth-dominance').innerHTML = `${ethDominance.toFixed(2)}%`;
+
+    // check if the fearGreedIndex object exists before accessing its value property
+    if (fearGreedIndex) {
+      document.getElementById('fear-greed-index').innerHTML = fearGreedIndex.value;
+    } else {
+      document.getElementById('fear-greed-index').innerHTML = 'N/A';
+    }
   })
   .catch(error => console.error(error));
